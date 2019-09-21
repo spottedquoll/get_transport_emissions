0e76bf3fa8b95b8fcb8ef595c8e2f9f9a818573b
supplychain-mac01 <supplychain-mac01@supplychain-mac01.local>
2019-09-21 09:43:46 +0900

    Added N20

--- edgar_emissions.py
+++ edgar_emissions.py
@@ -8,7 +8,7 @@ data_dir = transport_dir + '/edgar_data/'
 unit_scale = 1 * 10e9 * (1/10e6) * (1/10e6)  # Gg to MT
 emissions_files = ['v432_CO2_excl_short-cycle_org_C_1970_2012', 'v432_CO2_org_short-cycle_C_1970_2012'
                    , 'v432_PM2.5_fossil_1970_2012', 'v432_PM2.5_bio_1970_2012', 'v432_PM10_1970_2012'
-                   , 'v432_CH4_1970_2012', 'v432_NOx_1970_2012', 'v432_SO2_1970_2012']
+                   , 'v432_CH4_1970_2012', 'v432_NOx_1970_2012', 'v432_SO2_1970_2012', 'v432_N2O_1970_2012']
 
 for file in emissions_files:
 
@@ -37,7 +37,7 @@ for file in emissions_files:
     totals['all_other'] = (df[year].sum() * unit_scale) - running_total
 
     # Append to results container
-    totals['series'] = file
+    totals['filename'] = file
     results_df = results_df.append(totals, ignore_index=True)
 
 # export
